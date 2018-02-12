583decefed71e9acf515aa239c3321b290ad702d
jyoo980 <jyoo980@gmail.com>
2018-02-11 23:36:36 -0800

    Added support for 2 column pickerView

--- CryptoExchange/ViewController.swift
+++ CryptoExchange/ViewController.swift
@@ -8,21 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
     @IBOutlet weak var cryptoPicker: UIPickerView!
-    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let crypoCurrencies = [["BCH", "BTC", "BTG", "ETC", "LTC", "XRP", "ZEC"],
+                           ["CAN", "USD", "GBP"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cryptoPicker.delegate = self
+        cryptoPicker.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Number of rows in our pickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return crypoCurrencies.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return crypoCurrencies[component].count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent
+        component: Int) -> String? {
+        return crypoCurrencies[component][row]
+    }
+    
 }
 
