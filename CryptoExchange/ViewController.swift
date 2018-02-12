dd700d7b3606608b8a903e4acb975566ccd08f42
jyoo980 <jyoo980@gmail.com>
2018-02-11 22:42:21 -0800

    Initial commit

---
+++ CryptoExchange/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CryptoExchange
+//
+//  Created by James Yoo on 2018-02-11.
+//  Copyright © 2018 James Yoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cryptoPicker: UIPickerView!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
